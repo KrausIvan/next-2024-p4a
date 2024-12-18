bcb33aafcebba085224e1480d333518b12111ad4
Ivan Kraus <ivan.kraus@post.cz>
2024-12-18 15:05:40 +0100

    commit

--- nxt05-auth/components/server/LoggedUser.tsx
+++ nxt05-auth/components/server/LoggedUser.tsx
@@ -1,9 +1,13 @@
-import { auth } from "@/libs/auth"
+import { auth } from "@/libs/auth";
 
 const LoggedUser = async () => {
   const session = await auth();
-  console.log(session);
-  return session ? <>{session.user!.email}</> : <>Nepřihlášený uživatel</>
-}
+
+  return session?.user ? (
+      <p>Přihlášený uživatel: {session.user.email}</p>
+  ) : (
+      <p>Nepřihlášený uživatel</p>
+  );
+};
 
 export default LoggedUser;
--- nxt05-auth/libs/auth.ts
+++ nxt05-auth/libs/auth.ts
@@ -1,8 +1,23 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/libs/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
   ],
-})
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      if (user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
